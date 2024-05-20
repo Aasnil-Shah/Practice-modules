3792e9e29916dc59ce87a992457e19cda592c36a
Aasnil-Shah <aasnil.shah@slscorp.com>
2024-05-20 12:50:50 +0530

    Delete 8_c.py

--- 8_c.py
+++
@@ -1,4 +0,0 @@
-dict = {"name":"Plato","country":"Ancient Greece","born":-427,"teacher":"Socrates","student":"Aristotle"}
-
-dict["work"] =  ["Apology", "Phaedo", "Republic", "Symposium"]
-print(dict)
